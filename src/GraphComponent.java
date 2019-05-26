ff94fc51db97ee7bc320b761b3085cd783416696
huhnluka <lukas.nikolaus.huhn@student.uni-augsburg.de>
2019-05-26 19:02:43 +0200

    Plottin3

--- src/GraphComponent.java
+++ src/GraphComponent.java
@@ -17,7 +17,7 @@ public class GraphComponent extends JComponent{
 	@Override
 	public void paint(Graphics g){
 		super.paint(g);
-		g.setColor(Color.CYAN);
+		g.setColor(Color.WHITE);
 		g.fillRect(0, 0, getWidth(), getHeight());
 		
 		double minVal = calcMin(values);
@@ -27,19 +27,34 @@ public class GraphComponent extends JComponent{
 		int h = getHeight();
 		int w = getWidth();
 		
-		g.setColor(Color.BLACK);
+		
 		int steps = 10;
 		int x, y;
 		double value;
-		for(int i=0; i<size; i++){
-			x = w*i/steps;
-			value = values.get(i);	
-			y = (int)(h * (value-minVal) / (maxVal - minVal));
-			g.fillOval(x-2, y-2, 4, 4);
+		double v1, v2, x1, x2, y1, y2;
+		g.setColor(Color.GRAY);
+		for(int i=1; i<size; i++){
+			x1 = w*(i-1)/size;
+			x2 = w*i/size;
+			v1 = values.get(i-1);	
+			v2 = values.get(i);	
+			y1 = (int)(h * (v1-minVal) / (maxVal - minVal));
+			y2 = (int)(h * (v2-minVal) / (maxVal - minVal));
+			g.drawLine((int)x1, (int)y1, (int)x2, (int)y2);
+			//g.fillOval(x-2, y-2, 4, 4);
 		}
+		g.setColor(Color.BLACK);
+		for(int i=0; i<steps; i++){
+			x = 0;
+			y = h * i / steps;
+			value = minVal + (maxVal-minVal)*i/steps;
+			String str = new DecimalFormat("0.0#").format(value);
+			g.drawString(str, x, y);
+		}
+		System.out.println(size);
 	}
 	public void addValue(double val){
-		values.addElement(val);
+		values.add(val);
 	}
 	private double calcMax(Vector<Double> vec) {
 		double max = Integer.MIN_VALUE;
