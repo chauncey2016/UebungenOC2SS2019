0ba325bdcc91bbaebc84713f77ad4aa74e805f57
gerlinvi <victor.walter.gerling@student.uni-augsburg.de>
2019-06-30 22:15:09 +0200

    checkpoint presi2

--- Optimierung/plot_distr.py
+++ Optimierung/plot_distr.py
@@ -1,6 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = pd.read_csv('output/bb5_distr.txt')
-plt.plot(data)
+data = pd.read_csv('output/hillclimbing_blackbox_2.csv', sep='\t', header=None, skiprows=1)
+plt.plot(data[0])
+plt.show()
+plt.plot(data[2])
+plt.show()
+plt.plot(data[4])
+plt.show()
+plt.plot(data[6])
 plt.show()
