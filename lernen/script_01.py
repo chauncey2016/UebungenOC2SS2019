f00a0f527bdeab197bc4577512f94c1a1d584e31
Lukas <plattgewalzt@hotmail.de>
2019-07-07 21:52:47 +0200

    Blatt fertig

--- lernen/script_01.py
+++ lernen/script_01.py
@@ -1,7 +1,7 @@
 import gym
 import random
 import sys
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 env = gym.make('FrozenLake-v0')
 
@@ -36,6 +36,25 @@ def updateQ(state1, state2, action, reward):
     q[index1] += alpha * (reward + gamma * promise - q[index1])
     #print("update Q", index1, q[index1], reward, action)
 
+wins = 0
+
+for x in range(1000000):
+    action = env.action_space.sample()
+    if random.random() > epsilon:
+        action = bestAction(newState)
+    newState, reward, done, info = env.step(action)
+    if done:
+        if newState == 15:
+            reward = 1
+        else:
+            reward = -1
+        updateQ(oldState, newState, action, reward)
+        newState = env.reset()
+        oldState = newState
+    else:
+    	updateQ(oldState, newState, action, reward)
+    	oldState = newState
+
 def printQ():
 	for x in q:
 		sys.stdout.write("{:1.6f}".format(x))
@@ -58,65 +77,10 @@ def printBestActions():
 		if x % 4 == 0:
 			sys.stdout.write("\n")
 	sys.stdout.write("\n")
-
-def averageWinsPerEpisode(t_2, episodes, length):
-	wins = t_2[episodes]
-	if length > episodes:
-		return wins / (episodes+1)
-	oldWins = t_2[episodes - length]
-	return (wins - oldWins) / length
-
-wins = 0
-loos = 0
-movs = 0
-episodes = 0
-t = [0] * 100000
-t_2 = [0] * 100000
-t_3 = [0] * 100000
-t_4 = [0] * 100000
-t_5 = [0] * 100000
-for x in range(1000000):
-    action = env.action_space.sample()
-    if random.random() > epsilon:
-        action = bestAction(newState)
-    newState, reward, done, info = env.step(action)
-    if done:
-        if newState == 15:
-            wins = wins + 1
-            reward = 1
-        else:
-            loos = loos + 1
-            reward = -1
-        t[episodes] = x
-        t_2[episodes] = wins
-        t_3[episodes] = loos
-        t_4[episodes] = movs
-        t_5[episodes] = averageWinsPerEpisode(t_2, episodes, 100);
-        movs = 0
-        episodes = episodes + 1
-        updateQ(oldState, newState, action, reward)
-        newState = env.reset()
-        oldState = newState
-        #if episodes == 1000:
-        #    break
-        if t_5[episodes - 1] > 0.5:
-        	break;
-    else:
-        updateQ(oldState, newState, action, reward)
-        oldState = newState
-        movs = movs + 1
-
-env.render()
-
-printQ()
+#printQ()
 printBestActions()
-
-print("avg-Wins:", t_5[episodes - 1])
-print("episodes:", episodes)
-
 state = env.reset();
-
-for x in range(100):
+for x in range(500):
     input()
     env.render()
     action = bestAction(state)
@@ -126,19 +90,3 @@ for x in range(100):
     	env.render()
     	env.reset();
 env.close()
-
-e = episodes
-t = t[0:e]
-t_2 = t_2[0:e]
-t_3 = t_3[0:e]
-t_4 = t_4[0:e]
-t_5 = t_5[0:e]
-
-
-plt.plot(t_5)
-plt.show()
-plt.plot(t_2)
-plt.plot(t_3)
-plt.show()
-plt.plot(t_4)
-plt.show()
